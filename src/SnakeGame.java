57caa99e5c2f057a0f1969e0e03d3fec6ba9b4c6
peipey11 <peipey11@users.noreply.github.com>
2025-07-24 23:13:46 +0530

    Snake head

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -6,14 +6,49 @@ import javax.swing.*;
 
 
 public class SnakeGame extends JPanel{
+    private class Tile{
+        int x;
+        int y;
+
+        Tile(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     int boardHeight;
     int boardWidth;
+    int tileSize = 25;
+
+    Tile snakeHead;
 
     SnakeGame(int boardWidth, int boardHeight){
         this.boardWidth = boardWidth;
         this.boardHeight = boardHeight;
         setPreferredSize(new Dimension(this.boardWidth, this.boardHeight));
         setBackground(Color.black);
+
+        snakeHead = new Tile(5, 5);
+
     }
     
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
+    
+    }
+
+    public void draw(Graphics g){
+
+        //snake colour
+        g.setColor(Color.green);
+        g.fillRect(snakeHead.x, snakeHead.y, tileSize, tileSize);
+
+
+    }
+
+
+
+
+
 }
